a49b6ddf3765d14fa2c935b028e33fe6b2f5da78
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-08-25 11:29:36 -0500

    I added some more tests to account for higher capacity being added when the count rises and checking to make sure the items are added to the array all the 8 tests passed.

--- CustomListTesting/AddTesting.cs
+++ CustomListTesting/AddTesting.cs
@@ -34,6 +34,35 @@ namespace CustomListTesting
         }
 
         [TestMethod]
+        public void Add_AddSixValues_CountOf6()
+        {
+            {
+                //arrange
+
+                CustomList<int> customList = new CustomList<int>();
+                int value1 = 1;
+                int value2 = 2;
+                int expected = 6;
+                int actual;
+
+                //act
+                customList.Add(ref value1);
+                customList.Add(ref value2);
+                customList.Add(ref value1);
+                customList.Add(ref value2);
+                customList.Add(ref value1);
+                customList.Add(ref value2);
+                actual = customList.Count;
+
+                //assert
+                Assert.AreEqual(expected, actual);
+            }
+
+
+
+        }
+
+        [TestMethod]
         public void Add_AddTwoValues_CapacityOf4()
         {
             //arrange
@@ -46,6 +75,33 @@ namespace CustomListTesting
 
             //act
             customList.Add(ref value1);
+            customList.Add(ref value2);
+            actual = customList.Capacity;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_AddNineValues_CapacityOf16()
+        {
+            //arrange
+
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int expected = 16;
+            int actual;
+
+            //act
+            customList.Add(ref value1);
+            customList.Add(ref value2);
+            customList.Add(ref value1);
+            customList.Add(ref value2);
+            customList.Add(ref value1);
+            customList.Add(ref value2);
+            customList.Add(ref value1);
+            customList.Add(ref value2);
             customList.Add(ref value2);
             actual = customList.Capacity;
 
@@ -76,7 +132,7 @@ namespace CustomListTesting
         }
 
         [TestMethod]
-        public void Add_AddTwoValues_IndexAtFiveIsOne()
+        public void Add_AddSixValues_IndexAtFiveIsOne()
         {
             //arange
             CustomList<int> customList = new CustomList<int>();
@@ -114,6 +170,33 @@ namespace CustomListTesting
             customList.Add(ref value2);
             customList.Add(ref value1);
             actual = customList.Capacity;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Add_AddTenValues_CapacityAtIndexNineIsTwo()
+        {
+            //arange
+            CustomList<int> customList = new CustomList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int expected = 2;
+            int actual;
+            //act
+            customList.Add(ref value1);
+            customList.Add(ref value2);
+            customList.Add(ref value1);
+            customList.Add(ref value2);
+            customList.Add(ref value1);
+            customList.Add(ref value1);
+            customList.Add(ref value2);
+            customList.Add(ref value1);
+            customList.Add(ref value2);
+            customList.Add(ref value1);
+            actual = customList[8];
 
             //assert
             Assert.AreEqual(expected, actual);
